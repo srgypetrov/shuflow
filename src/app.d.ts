6e789a38dc47216c6a8837ee12e8e0443774eeb4
Sergey Petrov <sergey.petrov@rive.es>
2025-04-30 14:01:51 +0300

    Add PWA support

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,8 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+import 'vite-plugin-pwa/info'
+import 'vite-plugin-pwa/pwa-assets'
+
 declare global {
 	namespace App {
 		// interface Error {}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,25 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import { SvelteKitPWA } from '@vite-pwa/sveltekit'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit(),
+		SvelteKitPWA({
+			manifest: {
+				theme_color: '#22304E',
+				background_color: '#22304E',
+				icons: [
+					{
+						src: '/logo.svg',
+						sizes: 'any',
+						type: 'image/svg+xml'
+					}
+				],
+				name: 'Shuflow',
+				short_name: 'Shuflow'
+			}
+		})
+	],
 	server: { host: '127.0.0.1' }
 })
